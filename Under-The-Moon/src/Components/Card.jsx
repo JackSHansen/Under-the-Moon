0069a00b16505f352fac81ad633bbbb1f7a492d5
JackSHansen <jsh.1999@gmail.com>
2024-08-14 12:32:27 +0200

    ...
    ...

---
+++ Under-The-Moon/src/Components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './Card.module.scss';
+
+const Card = ({ title, description, buttonText, imageUri }) => {
+  return (
+    <div className= {style.card_container}>
+      <img
+        src={imageUri}
+        alt={title}
+        className= {style.card_image}
+      />
+      <div className= {style.card_content}>
+        <h2 className= {style.card_title}>{title}</h2>
+        <p className= {style.card_description}>{description}</p>
+        <button className= {style.card_button}>
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
