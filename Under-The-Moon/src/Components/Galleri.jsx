42878af368d19608380c7ab6e2e071fc80f2c47a
JackSHansen <jsh.1999@gmail.com>
2024-08-14 13:26:54 +0200

    ...
    ...

--- Under-The-Moon/src/Components/Galleri.jsx
+++ Under-The-Moon/src/Components/Galleri.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import './App.css'; // Din hoved-CSS-fil
-import GridItem from './GridItem';
 import img3 from '../Pictures/page_1_img_11.png'
 import img4 from '../Pictures/page_1_img_16.png'
 import img5 from '../Pictures/page_1_img_2.png'
@@ -14,24 +12,23 @@ import img12 from '../Pictures/page_1_img_4.png'
 import img13 from '../Pictures/page_1_img_1.png'
 import img14 from '../Pictures/page_1_img_3.png'
 import style from './Galleri.module.scss'
-import style from './GridItem.module.scss'
 
 
 const Galleri = () => {
   return (
-    <div className="parent">
-      <GridItem className= {Style.img3} imgSrc={img1} />
-      <GridItem className={Style.img3} imgSrc={img2} />
-      <GridItem className={Style.img3} imgSrc={img3} />
-      <GridItem className={Style.img3} imgSrc={img4} />
-      <GridItem className={Style.img3} imgSrc={img5} />
-      <GridItem className={Style.img3} imgSrc={img6} />
-      <GridItem className={Style.img3} imgSrc={img7} />
-      <GridItem className={Style.img3} imgSrc={img8} />
-      <GridItem className={Style.img3} imgSrc={img9} />
-      <GridItem className={Style.img3} imgSrc={img10} />
-      <GridItem className={Style.img3} imgSrc={img11} />
-      <GridItem className={Style.img3} imgSrc={img12} />
+    <div className= {style.parents}>
+      <img src={img3} alt="" className= {style.img3}/>
+      <img src={img4} alt="" className= {style.img3}/>
+      <img src={img5} alt="" className= {style.img3}/>
+      <img src={img6} alt="" className= {style.img3}/>
+      <img src={img7} alt="" className= {style.img3}/>
+      <img src={img8} alt="" className= {style.img3}/>
+      <img src={img9} alt="" className= {style.img3}/>
+      <img src={img10} alt="" className= {style.img3}/>
+      <img src={img11} alt="" className= {style.img3}/>
+      <img src={img12} alt="" className= {style.img3}/>
+      <img src={img13} alt="" className= {style.img3}/>
+      <img src={img14} alt="" className= {style.img3}/>
     </div>
   );
 };
