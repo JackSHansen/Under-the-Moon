07a998262e334f3b053f39aaac9f822c6fd45b95
JackSHansen <142877764+JackSHansen@users.noreply.github.com>
2024-08-15 11:13:23 +0200

    ...
    ...

--- Under-The-Moon/src/App.jsx
+++ Under-The-Moon/src/App.jsx
@@ -1,6 +1,5 @@
-import Undertext from './components/Undertext'
-
 import { useState } from 'react'
+import Undertext from './components/Undertext'
 import Galleri from './Components/Galleri'
 import MainComponent from './Components/MainComponent'
 import Header from './components/Header';
